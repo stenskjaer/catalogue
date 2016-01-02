6af8747009beaf3b5cc6798d8303e84ebec73941
Michael S. Christensen <michael.stenskjaer@gmail.com>
2016-01-02 17:09:33 +0100

    Move from local sqlite to remote postgresql

--- catalogue/settings.py
+++ catalogue/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'catalogue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catalogue',
+        'USER': 'michael',
+        'PASSWORD': '2Rm7iGJrlTIQ',
+        'HOST': 'catalogue.ctmgmgwqndgo.eu-west-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
