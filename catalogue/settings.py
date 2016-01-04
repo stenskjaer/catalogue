7ef7401cae3d214f00142a5ea49ea82853416a24
Michael S. Christensen <michael.stenskjaer@gmail.com>
2016-01-04 19:41:25 +0100

    Add static files to static base dir

--- catalogue/settings.py
+++ catalogue/settings.py
@@ -139,7 +139,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
