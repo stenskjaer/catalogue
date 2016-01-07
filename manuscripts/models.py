29e2b00de8e0a388725302dbab60504ec8b9a5b7
Michael S. Christensen <michael.stenskjaer@gmail.com>
2016-01-07 13:08:45 +0100

    Model Manuscript: Note field below annotation

--- manuscripts/models.py
+++ manuscripts/models.py
@@ -234,8 +234,8 @@ class Manuscript(BaseModel):
     folios = models.CharField(max_length=20, null=True, blank=True)
     layout = models.TextField(blank=True, null=True)
     script = models.TextField(blank=True, null=True)
-    note = models.TextField(blank=True, null=True)
     annotation = models.TextField(blank=True, null=True)
+    note = models.TextField(blank=True, null=True)
     literature = models.TextField(null=True, blank=True)
 
 
