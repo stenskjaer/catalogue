ff4e3510075486918af6681a66f89d60c14ba7b9
Michael S. Christensen <michael.stenskjaer@gmail.com>
2016-02-17 07:47:46 +0100

    Specify fields on manuscript form
    Mostly to change order of dimensions (height x width)

--- manuscripts/forms.py
+++ manuscripts/forms.py
@@ -5,4 +5,8 @@ from .models import Manuscript
 class ManuscriptForm(forms.ModelForm):
     class Meta:
         model = Manuscript
-        fields = '__all__'
+        fields = ['country', 'town', 'library', 'shelfmark', 'number',
+                  'olim', 'date', 'date_earliest', 'date_latest', 'date',
+                  'saeculo', 'origin', 'material', 'height', 'width',
+                  'folios', 'layout', 'script', 'annotation', 'note',
+                  'literature']
