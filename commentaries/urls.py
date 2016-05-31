160c5c7294f8e5db4b4fe3f0be9a931e59e6e17b
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2016-05-31 10:52:52 +0200

    Create view with all DA commentaries
    sorted by relevance

--- commentaries/urls.py
+++ commentaries/urls.py
@@ -6,5 +6,6 @@ app_name = 'commentaries'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.Details.as_view(), name='detail'),
-    url(r'^inspected/$', views.Inspected.as_view(), name='inspected')
+    url(r'^inspected/$', views.Inspected.as_view(), name='inspected'),
+    url(r'^relevant/$', views.Relevant.as_view(), name='relevant'),
 ]
--- commentaries/views.py
+++ commentaries/views.py
@@ -73,3 +73,40 @@ class Inspected(generic.ListView):
             text_list['unknown']
         )
         return(text_list)
+
+class Relevant(generic.ListView):
+    model = Text
+    template_name = 'texts/relevant.html'
+    context_object_name = 'texts'
+
+
+    def get_queryset(self):
+        # Read all texts (on De anima) into memory
+        all_texts = Text.objects.select_related().filter(commentary_on=148)
+
+        # Sort the list of relevant texts into relevance lists
+        text_list = {}
+        text_list['high_relevance'] = []
+        text_list['some_relevance'] = []
+        text_list['low_relevance']  = []
+        text_list['no_relevance']   = []
+        text_list['unknown'] = []
+        for text in all_texts:
+            if text.relevance == Text.RELEVANCE_HIGH:
+                text_list['high_relevance'].append(text)
+            elif text.relevance == Text.RELEVANCE_MID:
+                text_list['some_relevance'].append(text)
+            elif text.relevance == Text.RELEVANCE_LOW:
+                text_list['low_relevance'].append(text)
+            elif text.relevance == Text.RELEVANCE_NONE:
+                text_list['no_relevance'].append(text)
+            elif text.relevance == Text.RELEVANCE_UNKNOWN:
+                text_list['unknown'].append(text)
+        text_list['total_count'] = len(
+            text_list['high_relevance'] +
+            text_list['some_relevance'] +
+            text_list['low_relevance']  +
+            text_list['no_relevance']   +
+            text_list['unknown']
+        )
+        return(text_list)
