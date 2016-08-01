141b4c1080e47a0c2d2a1f46cb3ae763cd84dfce
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2016-08-01 13:59:19 +0200

    Move secrets into non-versioned file

--- catalogue/settings.py
+++ catalogue/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from . import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['CATALOGUE_DJANGO_KEY']
+SECRET_KEY = secrets.CATALOGUE_DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,7 +86,8 @@ WSGI_APPLICATION = 'catalogue.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DB_PASS = os.environ['CATALOGUE_DB_PASS']
+DB_PASS = secrets.CATALOGUE_DB_PASS
+
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
